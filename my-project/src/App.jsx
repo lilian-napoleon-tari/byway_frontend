00b03dbfaca4e10894f83657334db047a6a0fbc6
Lilian Napoleon Modupe <napoleonliliantari@outlook.com>
2025-05-10 23:27:29 +0100

    feat:project

---
+++ my-project/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router'
+
+const App = () => {
+  return (
+    <div className='min-hscreen'>App</div>
+  )
+}
+
+export default App
---
+++ my-project/src/pages/StudentLearningPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StudentLearningPage = () => {
+  return (
+    <div>StudentLearningPage</div>
+  )
+}
+
+export default StudentLearningPage
